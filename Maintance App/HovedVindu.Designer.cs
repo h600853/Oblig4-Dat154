060cc9ec1cda8b06f16f93a24bad1f54a2591a74
602822 <89077126+602822@users.noreply.github.com>
2023-04-24 21:12:12 +0200

    Knappene i hovudVinduet redirecter deg til riktige vindu

--- Maintance App/HovedVindu.Designer.cs
+++ Maintance App/HovedVindu.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            addOrDeleteButton = new Button();
+            editStatusButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Add or Delete";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(178, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Change Status";
+            // 
+            // addOrDeleteButton
+            // 
+            addOrDeleteButton.Location = new Point(24, 77);
+            addOrDeleteButton.Name = "addOrDeleteButton";
+            addOrDeleteButton.Size = new Size(142, 34);
+            addOrDeleteButton.TabIndex = 2;
+            addOrDeleteButton.Text = "Add or delete";
+            addOrDeleteButton.UseVisualStyleBackColor = true;
+            addOrDeleteButton.Click += addOrDelete_Click;
+            // 
+            // editStatusButton
+            // 
+            editStatusButton.Location = new Point(178, 77);
+            editStatusButton.Name = "editStatusButton";
+            editStatusButton.Size = new Size(112, 34);
+            editStatusButton.TabIndex = 3;
+            editStatusButton.Text = "Edit Status";
+            editStatusButton.UseVisualStyleBackColor = true;
+            editStatusButton.Click += editStatusButton_Click;
+            // 
+            // HovedVindu
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(editStatusButton);
+            Controls.Add(addOrDeleteButton);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "HovedVindu";
+            Text = "Form1";
+            Load += HovedVindu_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button addOrDeleteButton;
+        private Button editStatusButton;
     }
 }
--- Maintance App/HovedVindu.cs
+++ Maintance App/HovedVindu.cs
@@ -6,5 +6,28 @@ namespace Maintance_App
         {
             InitializeComponent();
         }
+
+        private void HovedVindu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Redirecter deg til AddandRemoveTask.cs når du trykker på knappen
+        private void addOrDelete_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AddandRemoveTask addorRemoveTask = new AddandRemoveTask();
+            addorRemoveTask.Show();
+
+        }
+
+        //Redirecter deg til EditStatus.cs når du trykker på knappen
+        private void editStatusButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            EditStatus editStatus = new EditStatus();
+            editStatus.Show();
+
+        }
     }
 }
